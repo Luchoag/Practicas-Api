92ea97a6233ed6e5566a3cd46f79a5e0455dfc93
Luciano Greppi <luchoag@gmail.com>
2020-08-23 13:07:37 -0300

    Actualizada información del proyecto

--- src/main/java/com/greppiluciano/practicasapi/PracticasApiApplication.java
+++ src/main/java/com/greppiluciano/practicasapi/PracticasApiApplication.java
@@ -7,7 +7,7 @@
  * Adicionalmente, se han agregado métodos con funcionalidad propia que realizan consultas SQL personalizadas y son capturadas por el RESTController.<br>
  * Las consultas que pueden realizarse a través de la API son las cuatro básicas de todo CRUD (leer, insertar, actualizar y eliminar) mediante los métodos GET, POST, PUT y DELETE,
  * y se añadieron además dos métodos propios que leen cada columna de la tabla por separado.
- * Las tecnologías utilizadas en el proyecto son: Java Spring Boot, JPA y MySQL.
+ * Las tecnologías utilizadas en el proyecto son: Java Spring Boot, JPA y MySQL. El IDE utilizado fue Spring Tool Suite 3.9.6.
  * 
  */
 
