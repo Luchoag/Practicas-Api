fa30cd2ab32149ac0fc5397b4464a34c8762b41a
Luciano Greppi <luchoag@gmail.com>
2020-08-23 12:51:32 -0300

    Se adiciona información sobre el proyecto

--- src/main/java/com/greppiluciano/practicasapi/PracticasApiApplication.java
+++ src/main/java/com/greppiluciano/practicasapi/PracticasApiApplication.java
@@ -2,9 +2,12 @@
  * @Luciano Greppi
  * Agosto 2020
  * 
- * Proyecto realizado con el fin de reforzar conocimientos sobre la creación de API REST.
- * El tutorial usado para modelar el proyecto puede encontrarse en: https://youtu.be/jO-o3qQZDuU
- * 
+ * Proyecto realizado con el fin de reforzar conocimientos sobre la creación de API REST. <br>
+ * El tutorial usado para modelar el proyecto puede encontrarse en: https://youtu.be/jO-o3qQZDuU <br>
+ * Adicionalmente, se han agregado métodos con funcionalidad propia que realizan consultas SQL personalizadas y son capturadas por el RESTController.<br>
+ * Las consultas que pueden realizarse a través de la API son las cuatro básicas de todo CRUD (leer, insertar, actualizar y eliminar) mediante los métodos GET, POST, PUT y DELETE,
+ * y se añadieron además dos métodos propios que leen cada columna de la tabla por separado.
+ * Las tecnologías utilizadas en el proyecto son: Java Spring Boot, JPA y MySQL.
  * 
  */
 
